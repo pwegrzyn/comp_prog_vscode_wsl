ba66ce173d074933781cf3aa50eb87ea5546903b
Patryk Wegrzyn <wegpat@gmail.com>
2021-06-27 23:00:47 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+
+#define all(x) (x).being() (x).end()
+#define pb push_back
+#define mp make_pair
+#define FILE "my_file"
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+const int INF = numeric_limits<int>::max();
+const ll LLINF = numeric_limits<ll>::max();
+const ull ULLINF = numeric_limits<ull>::max();
+const double PI = acos(-1.0);
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    // code here...
+}
